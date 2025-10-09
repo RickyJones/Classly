f37af75a9e5ced1265a500869d7ab5fbf2a9e5fb
rickyjones39 <ricky.jones@rctcbc.gov.uk>
2025-10-09 19:54:28 +0100

    study lab components

---
+++ Controllers/CommunityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Classly.Controllers
+{
+    public class CommunityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/StudyLabController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Classly.Controllers
+{
+    public class StudyLabController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
